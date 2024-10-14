2742dd02f416a5ff3e14a3911ccfaf95dbc02e94
David Alejandro Hernandez Rios <146588402+davidHdz1425@users.noreply.github.com>
2024-10-13 21:01:03 -0600

    Update stm32f446xx_usart_driver.c

--- stm32F446_uart/drivers/Src/stm32f446xx_usart_driver.c
+++ stm32F446_uart/drivers/Src/stm32f446xx_usart_driver.c
@@ -10,48 +10,42 @@
 void USART_SetBaudRate(USART_RegDef_t *pUSARTx, uint32_t BaudRate)
 {
 
-	//Variable to hold the APB clock
-	uint32_t PCLKx;
-
-	uint32_t usartdiv;
-
-	//variables to hold Mantissa and Fraction values
-	uint32_t M_part,F_part;
+  // PCLKx: hold the APB clock, M_part: hold Mantissa, Fpart: hold Fraction values
+  uint32_t PCLKx, usartdiv, M_part,F_part;
 
   uint32_t tempreg=0;
 
-  //Get the value of APB bus clock in to the variable PCLKx
+  // Get APB bus clock value 
   if(pUSARTx == USART1 || pUSARTx == USART6)
   {
-	   //USART1 and USART6 are hanging on APB2 bus
+	   // USART1 and USART6 on APB2 bus
 	   PCLKx = RCC_GetPCLCK2Value();
   }else
   {
 	   PCLKx = RCC_GetPCLCK1Value();
-
   }
 
-  //Check for OVER8 configuration bit
+  // Check for OVER8 configuration bit
   if(pUSARTx->CR1 & (1 << USART_CR1_OVER8))
   {
-	   //OVER8 = 1 , over sampling by 8
+	   // OVER8 = 1 , over sampling by 8
 	   usartdiv = ((25 * PCLKx) / (2 *BaudRate));
   }else
   {
-	   //over sampling by 16
+	   // Over sampling by 16
 	   usartdiv = ((25 * PCLKx) / (4 *BaudRate));
   }
 
-  //Calculate the Mantissa part
+  // Calculate the Mantissa part
   M_part = usartdiv/100;
 
-  //Place the Mantissa part in appropriate bit position . refer USART_BRR
+  // Place the Mantissa part in appropriate bit position . refer USART_BRR
   tempreg |= M_part << 4;
 
-  //Extract the fraction part
+  // Extract the fraction part
   F_part = (usartdiv - (M_part * 100));
 
-  //Calculate the final fractional
+  // Calculate the final fractional
   if(pUSARTx->CR1 & ( 1 << USART_CR1_OVER8))
    {
 	  //OVER8 = 1 , over sampling by 8
@@ -59,15 +53,15 @@ void USART_SetBaudRate(USART_RegDef_t *pUSARTx, uint32_t BaudRate)
 
    }else
    {
-	   //over sampling by 16
+	   // Over sampling by 16
 	   F_part = ((( F_part * 16)+ 50) / 100) & ((uint8_t)0x0F);
 
    }
 
-  //Place the fractional part in appropriate bit position . refer USART_BRR
+  // Place the fractional part in appropriate bit position . refer USART_BRR
   tempreg |= F_part;
 
-  //copy the value of tempreg in to BRR register
+  // Copy the value of tempreg in to BRR register
   pUSARTx->BRR = tempreg;
 }
 
@@ -80,44 +74,44 @@ void USART_Init(USART_Handle_t *pUSARTHandle)
 
 /******************************** Configuration of CR1******************************************/
 
-	//Implement the code to enable the Clock for given USART peripheral
+	// Implement the code to enable the Clock for given USART peripheral
 	 USART_PeriClockControl(pUSARTHandle->pUSARTx,ENABLE);
 
-	//Enable USART Tx and Rx engines according to the USART_Mode configuration item
+	// Enable USART Tx and Rx engines according to the USART_Mode configuration item
 	if ( pUSARTHandle->USART_Config.USART_Mode == USART_MODE_ONLY_RX)
 	{
-		//Implement the code to enable the Receiver bit field
+		// Implement the code to enable the Receiver bit field
 		tempreg|= (1 << USART_CR1_RE);
 	}else if (pUSARTHandle->USART_Config.USART_Mode == USART_MODE_ONLY_TX)
 	{
-		//Implement the code to enable the Transmitter bit field
+		// Implement the code to enable the Transmitter bit field
 		tempreg |= ( 1 << USART_CR1_TE );
 
 	}else if (pUSARTHandle->USART_Config.USART_Mode == USART_MODE_TXRX)
 	{
-		//Implement the code to enable the both Transmitter and Receiver bit fields
+		// Implement the code to enable the both Transmitter and Receiver bit fields
 		tempreg |= ( ( 1 << USART_CR1_RE) | ( 1 << USART_CR1_TE) );
 	}
 
-    //Implement the code to configure the Word length configuration item
+    // Implement the code to configure the Word length configuration item
 	tempreg |= pUSARTHandle->USART_Config.USART_WordLength << USART_CR1_M ;
 
 
-    //Configuration of parity control bit fields
+    // Configuration of parity control bit fields
 	if ( pUSARTHandle->USART_Config.USART_ParityControl == USART_PARITY_EN_EVEN)
 	{
-		//Implement the code to enable the parity control
+		// Implement the code to enable the parity control
 		tempreg |= ( 1 << USART_CR1_PCE);
 
-		//Implement the code to enable EVEN parity
-		//Not required because by default EVEN parity will be selected once you enable the parity control
+		// Implement the code to enable EVEN parity
+		// Not required because by default EVEN parity will be selected once you enable the parity control
 
 	}else if (pUSARTHandle->USART_Config.USART_ParityControl == USART_PARITY_EN_ODD )
 	{
-		//Implement the code to enable the parity control
+	    // Implement the code to enable the parity control
 	    tempreg |= ( 1 << USART_CR1_PCE);
 
-	    //Implement the code to enable ODD parity
+	    // Implement the code to enable ODD parity
 	    tempreg |= ( 1 << USART_CR1_PS);
 
 	}
